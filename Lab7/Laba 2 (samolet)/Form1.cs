ae58841974278fe4989ab986d280e5adad64e790
VladSelif <31828191+VladSelif@users.noreply.github.com>
2018-01-12 04:28:51 +0400

    Add files via upload

---
+++ Lab7/Laba 2 (samolet)/Form1.cs
@@ -0,0 +1,211 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_2__samolet_
+{
+    public partial class Form1 : Form
+    {
+
+        Parking parking;
+
+        Form2 form;
+
+        private Logger log;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            log = LogManager.GetCurrentClassLogger();
+            parking = new Parking(5);
+            
+
+            for (int i = 1; i < 6; i++)
+            {
+                listBox1.Items.Add("Уровень " + i);
+            }
+            listBox1.SelectedIndex = parking.getCurrentLevel;
+
+            Draw();
+        }
+
+        private void Draw()
+        {
+            if (listBox1.SelectedIndex > -1)
+            {
+
+
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                parking.Draw(gr);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void buttonSetAirplane_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var airplane = new Airplane(100, 4, 1000, dialog.Color);
+                int place = parking.PutAirplaneInParking(airplane);
+                Draw();
+                MessageBox.Show("№ Ангара: " + place);
+            }
+        }
+
+        private void buttonSetAerobus_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ColorDialog dialogDop = new ColorDialog();
+                if (dialogDop.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    var airplane = new Aerobus(100, 4, 1000, dialog.Color, true, true, true, true, dialogDop.Color);
+                    int place = parking.PutAirplaneInParking(airplane);
+                    Draw();
+                    MessageBox.Show("№ Ангара: " + place);
+                }
+            }
+
+
+        }
+
+        private void buttonTakeAirplane_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex > -1)
+            {// Прежде чем забрать машину, надо выбрать с какого уровня будем забирать
+                string level = listBox1.Items[listBox1.SelectedIndex].ToString();
+                if (maskedTextBox1.Text != "")
+                {
+                    log.Info("Забрали из ангара :" + Convert.ToInt32(maskedTextBox1.Text));
+                    
+                    try
+                    {
+                        ITransport airplane = parking.GetAirplaneInParking(Convert.ToInt32(maskedTextBox1.Text));
+
+                        Bitmap bmp = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+                        Graphics gr = Graphics.FromImage(bmp);
+                        airplane.setPosition(5, 5);
+                        airplane.drawAirplane(gr);
+                        pictureBox2.Image = bmp;
+                        Draw();
+                    }
+                    catch (ParkingOverflowException ex)
+                    {
+                        MessageBox.Show(ex.Message, "Неверный номер", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Общая ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        { }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            parking.LevelDown();
+            listBox1.SelectedIndex = parking.getCurrentLevel;
+            log.Info("Переход на этаж ниже: " + parking.getCurrentLevel);
+            Draw();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            parking.LevelUp();
+            listBox1.SelectedIndex = parking.getCurrentLevel;
+            log.Info("Переход на этаж выше: " + parking.getCurrentLevel);
+            Draw();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            form = new Form2();
+            form.AddEvent(AddAirplane);
+            form.Show();
+           
+        }
+
+        private void AddAirplane(ITransport airplane)
+        {
+            if (airplane != null)
+            {
+                try { 
+
+                int place = parking.PutAirplaneInParking(airplane);         
+                  Draw();
+                    log.Info("Добавление в ангар :" + place);
+                    MessageBox.Show("Ваше место: " + place);
+                }
+                catch (ParkingOverflowException ex)
+                {
+                    MessageBox.Show
+
+                        (ex.Message, "Ошибка переполнения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Общая ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (parking.SaveData(saveFileDialog1.FileName))
+                {
+                    MessageBox.Show("Сохранение прошло успешно", "",
+                         MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Не сохранилось", "",
+                         MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (parking.LoadData(openFileDialog1.FileName))
+                {
+                    MessageBox.Show("Загрузили", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+
+                    MessageBox.Show("Не загрузили", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                Draw();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
---
+++ Lab7/Laba 2 (samolet)/Parking.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_2__samolet_
+{
+    public class Parking
+    {
+
+        List<ClassArray<ITransport>> parkingStages;
+
+        int countPlaces = 15;
+
+        int placeSizeWidth = 210;
+
+        int placeSizeHeight = 100;
+
+        int currentLevel;
+
+        public int getCurrentLevel { get { return currentLevel; } }
+
+        public bool SaveData(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
+            {
+                using (BufferedStream bs = new BufferedStream(fs))
+                {
+                   
+                    byte[] info = new UTF8Encoding(true).GetBytes("CountLeveles:" +
+                    parkingStages.Count + Environment.NewLine);
+                    fs.Write(info, 0, info.Length);
+                    foreach (var level in parkingStages)
+                    {
+                        
+                        info = new UTF8Encoding(true).GetBytes("Level" + Environment.NewLine);
+                        fs.Write(info, 0, info.Length);
+                        for (int i = 0; i < countPlaces; i++)
+                        {
+                            var airplane = level[i];
+                            if (airplane != null)
+                            {
+                             
+                                if (airplane.GetType().Name == "Airplane")
+                                {
+                                    info = new UTF8Encoding(true).GetBytes("Airplane:");
+                                    fs.Write(info, 0, info.Length);
+                                }
+                                if (airplane.GetType().Name == "Aerobus")
+                                {
+                                    info = new UTF8Encoding(true).GetBytes("Aerobus:");
+                                    fs.Write(info, 0, info.Length);
+                                }
+                                
+                                info = new UTF8Encoding(true).GetBytes(airplane.getInfo() + Environment.NewLine);
+
+                                fs.Write(info, 0, info.Length);
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+
+        public bool LoadData(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                return false;
+            }
+            using (FileStream fs = new FileStream(filename, FileMode.Open))
+            {
+                string s = "";
+                using (BufferedStream bs = new BufferedStream(fs))
+                {
+                    byte[] b = new byte[fs.Length];
+                    UTF8Encoding temp = new UTF8Encoding(true);
+                    while (bs.Read(b, 0, b.Length) > 0)
+                    {
+                        s += temp.GetString(b);
+                    }
+                }
+                s = s.Replace("\r", "");
+                var strs = s.Split('\n');
+                if (strs[0].Contains("CountLeveles"))
+                {
+                    int count = Convert.ToInt32(strs[0].Split(':')[1]);
+                    if (parkingStages != null)
+                    {
+                        parkingStages.Clear();
+                    }
+                    parkingStages = new List<ClassArray<ITransport>>(count);
+                }
+                else
+                { 
+                    return false;
+                }
+                int counter = -1;
+                for(int i = 1; i < strs.Length; ++i)
+    
+                    {
+                        if (strs[i] == "Level")
+                        {
+                            counter++;
+                            parkingStages.Add(new ClassArray<ITransport>(countPlaces, null));
+                        }
+                        else if (strs[i].Split(':')[0] == "Airplane")
+                        {
+                            ITransport airplane = new Airplane(strs[i].Split(':')[1]);
+                            int number = parkingStages[counter] + airplane;
+                            if (number == -1)
+                            {
+                                return false;
+                            }
+                        }
+                        else if (strs[i].Split(':')[0] == "Aerobus")
+                        {
+                            ITransport airplane = new Aerobus(strs[i].Split(':')[1]);
+                            int number = parkingStages[counter] + airplane;
+                            if (number == -1)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+            }
+            return true;
+        }
+
+        public void LevelUp()
+        {
+            if (currentLevel + 1 < parkingStages.Count)
+            {
+                currentLevel++;
+            }
+        }
+
+
+        public void LevelDown()
+        {
+            if (currentLevel > 0)
+            {
+                currentLevel--;
+            }
+        }
+
+        public Parking(int countStages)
+        {
+            parkingStages = new List<ClassArray<ITransport>>(countStages);
+            for  (int i = 0; i < countStages; i++)
+            {
+                parkingStages.Add(new ClassArray<ITransport>(countPlaces, null));
+            }
+        }
+
+        public int PutAirplaneInParking (ITransport airplane)
+        {
+            return parkingStages[currentLevel] + airplane;
+        }
+
+        public ITransport GetAirplaneInParking(int ticket)
+        {
+            return parkingStages[currentLevel] - ticket;
+        }
+
+        public void Draw(Graphics g)
+        {
+            DrawMarking(g);
+            for (int i = 0; i < countPlaces; i++)
+            {
+                var airplane = parkingStages[currentLevel][i];
+                if (airplane != null)
+                { 
+                    airplane.setPosition(5 + i / 5 * placeSizeWidth + 5, i % 5 * placeSizeHeight + 15);
+                    airplane.drawAirplane(g);
+                }
+            }
+        }
+
+        private void DrawMarking(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black, 3);
+            g.DrawString("L" + (currentLevel + 1), new Font("Arial", 30), new SolidBrush(Color.Blue), (countPlaces / 5) * placeSizeWidth - 70, 420);
+            g.DrawRectangle(pen, 0, 0, (countPlaces / 5) * placeSizeWidth, 550);
+            for (int i = 0; i < countPlaces / 5; i++)
+            {
+
+                for (int j = 0; j < 6; ++j)
+                {
+                    g.DrawLine(pen, i * placeSizeWidth, j * placeSizeHeight, i * placeSizeWidth + 110, j * placeSizeHeight);
+                    if (j < 5)
+                    {
+                        g.DrawString((i * 5 + j + 1).ToString(), new Font("Arial", 30),
+                        new SolidBrush(Color.Blue), i * placeSizeWidth + 30, j * placeSizeHeight + 20);
+                    }
+                }
+                g.DrawLine(pen, i * placeSizeWidth, 0, i * placeSizeWidth, 502);
+            }
+        }
+    }
+                        
+}
